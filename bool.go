36fff45959c4468fe6c8b23b6fc115580c62498d
DESKTOP-9KA65BL\gogogo <511268932@qq.com>
2024-08-26 16:39:45 +0800

    pflag

--- bool.go
+++ bool.go
@@ -32,4 +32,26 @@ func boolConv(sval string) (interface{}, error) {
 	return strconv.ParseBool(sval)
 }
 
-func (f *FlagSet)
+func (f *FlagSet) GetBool(name string) (bool, error) {
+	val, err := f.getFlagType(name, "bool", boolConv)
+	if err != nil {
+		return false, err
+	}
+	return val.(bool), nil
+}
+
+func (f *FlagSet) BoolVar(p *bool, name string, value bool, usage string) {
+	f.BoolVarP(p, name, "", value, usage)
+}
+
+func (f *FlagSet) BoolVarP(p *bool, name, shorthand string, value bool, usage string) {
+	flag := f.VarPF(newBoolValue(value, p), name, shorthand, usage)
+	flag.NoOptDefVal = "true"
+}
+
+func BoolVarP(p *bool, name, shorthand string, value bool, usage string) {
+	flag := CommandLine.VarPF(newBoolValue(value, p), name, shorthand, usage)
+	flag.NoOptDefVal = "true"
+}
+
+
